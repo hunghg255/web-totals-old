371b7a32e1ad137ff377bdb7fca9c609d99f66b4
heesungjang <heesungj7@gmail.com>
2022-08-31 14:37:45 +0900

    questions added 11~20

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
 function Question({ render, children }) {
+  if (!children) {
+    return (
+      <div className="lg:flex lg:flex-row items-center">
+        {/* markdown */}
+        {/* <div className="w-[100%] mr-4">{children}</div> */}
+        {/* answers */}
+        <div className="flex flex-col 2 justify-between w-full">{render}</div>
+      </div>
+    )
+  }
   return (
     <div className="lg:flex lg:flex-row items-center">
       {/* markdown */}
---
+++ components/Summary.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Summary() {
+  return (
+    <summary>
+      <b>💡 Answer</b>
+    </summary>
+  )
+}
+
+export default Summary
