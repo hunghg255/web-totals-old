56d1230de760f3e3bf7d8cc8d132f76fe76bc5e6
hunghg255 <giahunghust@gmail.com>
2023-07-12 11:50:09 +0700

    update

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Layout from '@theme-original/Layout';
 import Plum from '@site/src/components/Plum/Plum';
 import BrowserOnly from '@docusaurus/BrowserOnly';
+import LogArt from '@site/src/components/LogArt/LogArt';
 
 export default function LayoutWrapper(props) {
   return (
     <>
       <BrowserOnly fallback={<>Loading...</>}>{() => <Plum />}</BrowserOnly>
+      <BrowserOnly fallback={<>Loading...</>}>{() => <LogArt />}</BrowserOnly>
       <Layout {...props} />
     </>
   );
