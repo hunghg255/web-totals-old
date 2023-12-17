accfbc130d4f6391ded21f8b1df429cc4ecd57d8
hunghg255 <giahunghust@gmail.com>
2023-12-18 01:59:46 +0700

    chore: add hightlight shikiji

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -3,10 +3,11 @@ import Layout from '@theme-original/Layout';
 import Plum from '@site/src/components/Plum/Plum';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import LogArt from '@site/src/components/LogArt/LogArt';
-
+import Test from '@site/src/components/Test';
 export default function LayoutWrapper(props) {
   return (
     <>
+      <BrowserOnly fallback={<></>}>{() => <Test />}</BrowserOnly>
       <BrowserOnly fallback={<></>}>{() => <Plum />}</BrowserOnly>
       <BrowserOnly fallback={<></>}>{() => <LogArt />}</BrowserOnly>
       <Layout {...props} />
